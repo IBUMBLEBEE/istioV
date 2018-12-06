ef2ceb24d926e4e9bac6e27bf1e794c657e5880f
田俊 <tianjun@codoon.com>
2018-12-06 15:32:22 +0800

    add func

---
+++ funclab_k8s.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"runtime"
+	"strconv"
+	"third/gin"
+
+	"github.com/google/gopacket/pcap"
+)
+
+func main() {
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+		fmt.Println("The number of ping: ", runtime.NumGoroutine(), GetGID())
+	})
+
+	router.GET("/num/goroutine", func(c *gin.Context) {
+		go func() {
+			fmt.Println("The number of goroutine: ", runtime.NumGoroutine(), GetGID())
+		}()
+	})
+
+	// func group
+	go FindDevices()
+
+	router.Run(":9180")
+}
+
+func FindDevices() {
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println("Devices found:")
+	for _, device := range devices {
+		fmt.Println("\n Name", device.Name)
+		fmt.Println("Descripttion: ", device.Description)
+		fmt.Println("Devices address: ", device.Addresses)
+		for _, address := range device.Addresses {
+			fmt.Println("- IP address: ", address.IP)
+			fmt.Println("- Subnet mask: ", address.Netmask)
+		}
+	}
+}
+
+func GetGID() uint64 {
+	// fmt.Println("The number of GetGID: ", runtime.NumGoroutine(), GetGID())
+	b := make([]byte, 64)
+	b = b[:runtime.Stack(b, false)]
+	b = bytes.TrimPrefix(b, []byte("goroutine "))
+	b = b[:bytes.IndexByte(b, ' ')]
+	n, _ := strconv.ParseUint(string(b), 10, 64)
+	return n
+}
