a3ef46661c8e252db511fffe98ac7382ef8cbe4b
田俊 <tianjun@codoon.com>
2018-12-07 15:52:15 +0800

    decode packet

--- funclab_k8s.go
+++ funclab_k8s.go
@@ -6,7 +6,10 @@ import (
 	"log"
 	"runtime"
 	"strconv"
+	"strings"
 	"time"
+
+	"github.com/google/gopacket/layers"
 
 	"github.com/google/gopacket"
 
@@ -82,11 +85,78 @@ func OpenDeviceLiveCapture() {
 		packageSource := gopacket.NewPacketSource(handle, handle.LinkType())
 		fmt.Println(packageSource)
 		for packet := range packageSource.Packets() {
-			go func() {
-				fmt.Println(packet)
-			}()
+			printPacketInfo(packet)
+			time.Sleep(5 * time.Second)
 		}
 	}
+}
+
+func printPacketInfo(packet gopacket.Packet) {
+	ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
+	if ethernetLayer != nil {
+		fmt.Println("Ethernet layer detected")
+		ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
+		fmt.Println("Source MAC: ", ethernetPacket.SrcMAC)
+		fmt.Println("Destination MAC: ", ethernetPacket.DstMAC)
+		fmt.Println("Ethernet type: ", ethernetPacket.EthernetType)
+		fmt.Println()
+	}
+
+	// Let's see if the packet is IP (even though the ether type told us)
+	ipLayer := packet.Layer(layers.LayerTypeIPv4)
+	if ipLayer != nil {
+		fmt.Println("IPV4 layer detected")
+		ip, _ := ipLayer.(*layers.IPv4)
+
+		// IP layer variables:
+		// version (Either 4 or 6)
+		// IHL (IP Header Length in 32-bit words)
+		// TOS, Length, Id, Flags, FragOffset, TTL, Procotol (TCP?)
+		// Checksum, srcIP, DstIP
+		fmt.Printf("From %s to %s\n", ip.SrcIP, ip.DstIP)
+		fmt.Println("protocol: ", ip.Protocol)
+		fmt.Println()
+	}
+
+	// Let's see if the packet is TCP
+	tcpLayer := packet.Layer(layers.LayerTypeTCP)
+	if tcpLayer != nil {
+		fmt.Println("TCP layer detected")
+		tcp, _ := tcpLayer.(*layers.TCP)
+
+		// TCP layer variables:
+		// SrcPort, DstPort, Seq, Ack, DataOffset, Window, Checksum, Urgent
+		// Bool flags: FIN, SYN, RST, PSH, ACK, URG, ECE, CWR, NS
+		fmt.Printf("From port %s to %d\n", tcp.SrcPort, tcp.DstPort)
+		fmt.Println("Sequence number: ", tcp.Seq)
+		fmt.Println()
+	}
+
+	// Iterate over all layer, printing out each layer type
+	fmt.Println("All packet layers:")
+	for _, layer := range packet.Layers() {
+		fmt.Println("- ", layer.LayerType())
+	}
+
+	// When iterating through packet.Layers() above,
+	// if it lists Payload layer then that is the same as
+	// the applicationLayer. applicationLayer contains the payload
+	applicationLayer := packet.ApplicationLayer()
+	if applicationLayer != nil {
+		fmt.Println("Application layer/Payload found.")
+		fmt.Printf("%s\n", applicationLayer.Payload())
+
+		// Search for a string inside the payload
+		if strings.Contains(string(applicationLayer.Payload()), "HTTP") {
+			fmt.Println("HTTP found!")
+		}
+	}
+
+	// Check for errors
+	if err := packet.ErrorLayer(); err != nil {
+		fmt.Println("Error decoding some part of the packet: ", err)
+	}
+	fmt.Println("========================================================================")
 }
 
 // GetGID get goroutine ID
